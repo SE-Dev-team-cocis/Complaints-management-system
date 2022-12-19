ab419a1c67ccfd1706e8d93e3eecb06cf998c60f
Jacobson <romeojacobson@gmail.com>
2022-12-19 13:30:23 +0300

    Updated header, footer, body background color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>This is our Footer</div>
+    <footer className='bg-dark p-3 w-100 h-30 text-light text-center'>
+      <p>&copy; Copyright 2022 | All rights reserved </p>
+    </footer>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
+
 function Header() {
+  let username = 'Loorinho';
   return (
-    <div>This is our navigation bar</div>
+    // <div>This is our navigation bar</div>
+    <header className='bg-light w-100 h-15 p-3 d-flex justify-content-between align-items-center'>
+      <div className='site_logo d-flex justify-content-around align-items-center' >
+        <img src="muklogo.jpg" alt="logo" className='me-5 muk_logo'/>
+        <p className=''>CMS</p>
+      </div>
+
+      <div className=''>
+        Hi, <span className='me-4'> {username}</span>
+        <button className='btn btn-outline-success'>Logout</button>
+      </div>
+    </header>
+   
   )
 }
 
